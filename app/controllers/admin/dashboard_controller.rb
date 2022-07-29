5799083d7e8c2634b2f1b3dab622d8183b39787a
SyedHamzaAsim <hamza.asim090@gmail.com>
2022-07-29 18:32:12 +0000

    Admin index

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,5 +1,7 @@
 class Admin::DashboardController < ApplicationController
   http_basic_authenticate_with name: ENV["HTTP_AUTH_USER"], password: ENV["HTTP_AUTH_PASS"]
   def show
+    @category = Category.all.count
+    @product = Product.all.count
   end
 end

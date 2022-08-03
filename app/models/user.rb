cac74b9e3d01f5c8c8a72eb71c7bf496cf7f2bf6
SyedHamzaAsim <hamza.asim090@gmail.com>
2022-08-03 04:13:01 +0000

    User and product tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,19 @@
 class User < ApplicationRecord
   has_secure_password
+
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :email, presence: true, uniqueness: { case_sensitive: false }
+  validates :password, presence: true, length: { in: 6..20 }
+  validates :password_confirmation, presence: true
+
+  
+  def self.authenticate_with_credentials(email_address, password)
+    @user = User.find_by_email(email_address)
+    if @user && @user.authenticate(password) 
+      @user
+    else
+      nil
+    end
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,73 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "Validations" do
+
+    before(:each) do
+      @user = User.create(first_name: "Hamza", last_name: "Asim", email: "test@test.com", password: "hamza123", password_confirmation: "hamza123")
+    end
+
+    it "registration will be vaild when all the required field are full" do
+      expect(@user).to be_valid
+    end
+    
+    it "email should be unique and not case sensitive" do
+      @newUser = User.create(first_name: "hyusadh", last_name: "Asim", email: "TEST@TEST.com", password: "hasbdc", password_confirmation: "hasbdc")
+      expect(@newUser).to be_invalid
+      expect(@newUser.errors.full_messages).to include("Email has already been taken")
+    end
+
+    it "email shoud not be blank" do
+      @user.email = nil
+      expect(@user).to be_invalid
+      expect(@user.errors.full_messages).to include("Email can't be blank")
+    end
+
+    it "first_name should not be blank" do
+      @user.first_name = nil
+      expect(@user).to be_invalid
+      expect(@user.errors.full_messages).to include("First name can't be blank")
+    end
+    
+    it "password and password_confirmation should match" do
+      @user.password = nil
+      @user.password_confirmation = nil
+      expect(@user).to be_invalid
+      expect(@user.errors.full_messages).to include("Password can't be blank")
+      @user.password = "hamza123"
+      @user.password_confirmation = "123456"
+      expect(@user).to be_invalid
+      expect(@user.errors.full_messages).to include("Password confirmation doesn't match Password")
+    end
+
+    it "password minimun length should be 6 character" do
+      @user.password = "abcde"
+      @user.password_confirmation = "abcde"
+      expect(@user).to be_invalid
+      expect(@user.errors.full_messages).to include("Password is too short (minimum is 6 characters)")
+    end
+
+  end
+
+  describe '.authenticate_with_credentials' do
+    it "valid email and password" do
+      @user = User.create(first_name: "Hamza", last_name: "Asim", email: "test@test.com", password: "abcde", password_confirmation: "abcde")
+      user1 = User.authenticate_with_credentials("test@test.com", "abcde")
+      expect(user1).to be_present
+    end
+    it "need vaild email" do
+      user = User.authenticate_with_credentials("telol@b.com", "abcde")
+      expect(user).to be_nil
+    end
+
+    it "user pass even enter capital letter in email" do
+      user = User.authenticate_with_credentials("tesTlol@b.com", "abcde")
+      expect(user).to be_present
+    end
+
+    it "user pass even enter white space before or after email" do
+      user = User.authenticate_with_credentials(" testlol@b.com ", "abcde")
+      expect(user).to be_present
+    end
+  end
 end
